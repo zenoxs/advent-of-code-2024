9bd40e89b6817519b213eeaadb459522c7224238
zenoxs <amaury.civier@gmail.com>
2024-12-09 09:28:09 +0100

    day-8

--- day-8/part2.ts
+++ day-8/part2.ts
@@ -1,5 +1,7 @@
 import * as path from "jsr:@std/path"
 import { Vector } from "vecti"
+import chalk from "chalk"
+import process from "node:process"
 
 const inputPath = path.join(import.meta.dirname!, "input.txt")
 const inputs = await Deno.readTextFile(inputPath)
@@ -8,19 +10,21 @@ const rows = inputs.split("\n").map((row) => row.split(""))
 
 function displayMap(rows: string[][], antinodes: Vector[]) {
     rows.forEach((row, y) => {
-        let line = ""
         row.forEach((cell, x) => {
-            if (cell !== ".") {
-                line += cell
-                return
+            const isAntenna = cell !== "."
+            const isAntinode = antinodes.some((a) => a.x === x && a.y === y)
+
+            if (isAntenna && isAntinode) {
+                process.stdout.write(chalk.bgBlue(cell))
+            } else if (isAntenna) {
+                process.stdout.write(cell)
+            } else if (isAntinode) {
+                process.stdout.write("#")
+            } else {
+                process.stdout.write(cell)
             }
-            if (antinodes.some((a) => a.x === x && a.y === y)) {
-                line += "#"
-                return
-            }
-            line += cell
         })
-        console.log(line)
+        process.stdout.write("\n")
     })
 }
 
@@ -43,8 +47,8 @@ displayMap(rows, [])
 
 const antinodes: Vector[] = []
 
-const maxY = rows.length - 1
-const maxX = rows[0].length - 1
+const height = rows.length - 1
+const width = rows[0].length - 1
 
 for (const [, antennas] of Object.entries(antennaDict)) {
     for (const antenna of antennas) {
@@ -54,23 +58,24 @@ for (const [, antennas] of Object.entries(antennaDict)) {
                 continue
             }
 
-            const diff = antenna.subtract(antennaComp)
-            const antinode = antenna.add(diff)
+            const delta = antenna.subtract(antennaComp)
 
-            if (antinode.x >= 0 && antinode.x <= maxX && antinode.y >= 0 && antinode.y <= maxY) {
-                // Sanity check to verify that the antinode is not already added
-                if (antinodes.some((a) => a.x === antinode.x && a.y === antinode.y)) {
-                    console.log(`${antinode} already added, should not happen`)
-                    continue
+            const maxAxis = Math.max(width, height)
+            for (let k = -maxAxis; k <= maxAxis; k++) {
+                const antinode = antenna.add(delta.multiply(k))
+
+                if (antinode.x >= 0 && antinode.x <= width && antinode.y >= 0 && antinode.y <= height) {
+                    antinodes.push(antinode)
                 }
-                antinodes.push(antinode)
             }
         }
     }
 }
 
 console.log("-----------------")
-
 displayMap(rows, antinodes)
 
-console.log(antinodes.length)
+console.log(
+    antinodes.filter((value, index, array) => array.findIndex((v) => v.x === value.x && v.y === value.y) === index)
+        .length,
+)
